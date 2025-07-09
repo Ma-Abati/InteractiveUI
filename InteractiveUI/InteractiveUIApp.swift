5cfd4d2a2493dca36e4c8bd6c35b9d7931a593d6
Maia Fattori Abati <maiaabati@gmail.com>
2025-07-09 14:15:01 -0300

    Initial Commit

---
+++ InteractiveUI/InteractiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIApp.swift
+//  InteractiveUI
+//
+//  Created by Maia Fattori Abati on 09/07/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
