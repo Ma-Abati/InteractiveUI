6cc1bfdfe50d5347daa960b2613713fe69c2a2f2
Maia Fattori Abati <maiaabati@gmail.com>
2025-07-09 14:25:50 -0300

    Core od code. Button, text and style

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -12,6 +12,18 @@ struct ContentView: View {
         VStack {
             Text("What is your name?")
                 .font(.title)
+            
+            TextField("type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 4)
+            
+            Button("Summit name") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
         }
         .padding()
     }
